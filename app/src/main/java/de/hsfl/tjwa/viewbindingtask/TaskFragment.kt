ae6dc56fef84e1731095ca0cc3c08b86acbba219
Tjorben Wade <gorb98@gmail.com>
2022-04-26 00:31:06 +0200

    Upload solution

--- app/src/main/java/de/hsfl/tjwa/viewbindingtask/TaskFragment.kt
+++ app/src/main/java/de/hsfl/tjwa/viewbindingtask/TaskFragment.kt
@@ -1,18 +1,24 @@
 package de.hsfl.tjwa.viewbindingtask
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import de.hsfl.tjwa.viewbindingtask.databinding.FragmentTaskBinding
 
 
 class TaskFragment : Fragment() {
 
     private lateinit var taskViewModel: TaskViewModel
+
+    private var _binding: FragmentTaskBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,18 +26,19 @@ class TaskFragment : Fragment() {
     ): View {
         taskViewModel = ViewModelProvider(requireActivity()).get(TaskViewModel::class.java)
 
-        val rootView: View = inflater.inflate(R.layout.fragment_task, container, false)
+        _binding = FragmentTaskBinding.inflate(inflater, container, false)
 
-        val bSaveTask = rootView.findViewById<TextView>(R.id.bSaveTask)
-        val etTask = rootView.findViewById<EditText>(R.id.etTask)
-        val tvCurrentTask = rootView.findViewById<TextView>(R.id.tvCurrentTask)
-
-        bSaveTask.setOnClickListener {
-            taskViewModel.currentTask = etTask.text.toString()
-            tvCurrentTask.text = taskViewModel.currentTask
+        val taskObserver = Observer<String> {
+            //onChange
+            binding.tvCurrentTask.text = it
+            Log.i("TaskFragment", "Neuer Wert: $it")
         }
-        tvCurrentTask.text = taskViewModel.currentTask
+        taskViewModel.currentTask.observe(viewLifecycleOwner, taskObserver)
 
-        return rootView
+        binding.bSaveTask.setOnClickListener {
+            taskViewModel.currentTask.value = binding.etTask.text.toString()
+        }
+
+        return binding.root
     }
 }
--- app/src/main/java/de/hsfl/tjwa/viewbindingtask/TaskViewModel.kt
+++ app/src/main/java/de/hsfl/tjwa/viewbindingtask/TaskViewModel.kt
@@ -1,15 +1,12 @@
 package de.hsfl.tjwa.viewbindingtask
 
-import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class TaskViewModel : ViewModel() {
 
-    var currentTask: String = "keine Aufgabe"
-        set(value) {
-            Log.i("TaskViewModel", "Neuer Wert: $value")
-            field = value
-        }
+    val currentTask: MutableLiveData<String> by lazy {
+        MutableLiveData<String>("keine Aufgabe")
+    }
+
 }
